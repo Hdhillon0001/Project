015e1b9ffb9625407b3aff12fc2197865237c488
Neeraj <neerarjathi81@gmail.com>
2020-07-04 17:40:15 +0530

    Initial commit

---
+++ app/src/main/java/com/project/model/Upload.java
@@ -0,0 +1,28 @@
+package com.project.model;
+
+
+public class Upload {
+
+    String userId, url;
+
+    public Upload(String userId, String url) {
+        this.userId = userId;
+        this.url = url;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
